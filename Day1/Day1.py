fbe3f77839e3d253e34f72e24ce3e2b6f3f0bb1a
Alex Gidman <alexgidman@outlook.com>
2020-12-02 21:53:58 +0000

    remove redundant condition

--- Day1/Day1.py
+++ Day1/Day1.py
@@ -12,14 +12,12 @@ with open('data.txt', 'r') as f:
     # Part 1
     # for i in range(len(puzzle_input)):
     #     for j in range(i+1, len(puzzle_input)):
-    #         if i != j:
-    #             if (puzzle_input[i] + puzzle_input[j]) == 2020:
-    #                 print(puzzle_input[i] * puzzle_input[j])
+    #         if (puzzle_input[i] + puzzle_input[j]) == 2020:
+    #             print(puzzle_input[i] * puzzle_input[j])
 
     # Part 2
     for i in range(len(puzzle_input)):
         for j in range(i+1, len(puzzle_input)):
             for k in range(j+1, len(puzzle_input)):
-                if i != j and j != k:
-                    if (puzzle_input[i] + puzzle_input[j] + puzzle_input[k]) == 2020:
-                        print(puzzle_input[i] * puzzle_input[j] * puzzle_input[k])
+                if (puzzle_input[i] + puzzle_input[j] + puzzle_input[k]) == 2020:
+                    print(puzzle_input[i] * puzzle_input[j] * puzzle_input[k])
