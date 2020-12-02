ae739fe69d9a6832cb61bae90e18ed8c499bfa9a
Alex Gidman <alexgidman@outlook.com>
2020-12-02 20:36:24 +0000

    add part 2 solution

--- Day1/Day1.py
+++ Day1/Day1.py
@@ -9,8 +9,17 @@ with open('data.txt', 'r') as f:
             break
         puzzle_input.append(int(line.strip()))
 
+    # Part 1
+    # for i in range(len(puzzle_input)):
+    #     for j in range(i+1, len(puzzle_input)):
+    #         if i != j:
+    #             if (puzzle_input[i] + puzzle_input[j]) == 2020:
+    #                 print(puzzle_input[i] * puzzle_input[j])
+
+    # Part 2
     for i in range(len(puzzle_input)):
-        for j in range(len(puzzle_input)):
-            if i != j:
-                if (puzzle_input[i] + puzzle_input[j]) == 2020:
-                    print(puzzle_input[i] * puzzle_input[j])
+        for j in range(i+1, len(puzzle_input)):
+            for k in range(j+1, len(puzzle_input)):
+                if i != j and j != k:
+                    if (puzzle_input[i] + puzzle_input[j] + puzzle_input[k]) == 2020:
+                        print(puzzle_input[i] * puzzle_input[j] * puzzle_input[k])
