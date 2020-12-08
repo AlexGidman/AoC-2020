e1a238a5d391996e2619d9260b562ee4ba9aea74
Alex Gidman <alexgidman@outlook.com>
2020-12-08 22:21:04 +0000

    added part 2 - be careful of pointers...

--- Day8/Day8.py
+++ Day8/Day8.py
@@ -29,20 +29,43 @@ def evaluate_action(action: list):
     if action[0] == 'jmp':
         return action[1]
 
+def run_actions_once(code, action_to_modify=None):
+    global acc
+    acc = 0
+    if action_to_modify:
+        if code[action_to_modify][0] == 'jmp':
+            code[action_to_modify][0] = 'nop'
+        elif code[action_to_modify][0] == 'nop':
+            code[action_to_modify][0] = 'jmp'
 
+    i = 0
+    completed_actions = []
+    while(True):
+        if i == len(code)-1:
+            evaluate_action(code[i])
+            print(f"acc: {acc}")
+            completed_actions.append(i)
+            break
+        elif i not in completed_actions:
+            completed_actions.append(i)
+            i += evaluate_action(code[i])
+        else:
+            # print(f"acc: {acc}")
+            break
+    return completed_actions
+
+# Read in data
 code = read_in_data('data.txt')
 
 # PART1
-i = 0
-completed_actions = []
-while(i < len(code)):
-    if i not in completed_actions:
-        completed_actions.append(i)
-        i += evaluate_action(code[i])
-    else:
-        print(acc)
-        break
+completed_actions = run_actions_once(code=code)
+
+# PART 2
+for i in range(len(code)):
+    code = read_in_data('data.txt')
+    completed_actions = run_actions_once(code=code, action_to_modify=i)
+    if (len(code)-1) in completed_actions:
+        print(f"{i} {code[i]}")
 
 
 
-    
