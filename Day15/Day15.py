392e8502c14288d1b806bd926a0c090c1928e5a4
Alex Gidman <alexgidman@outlook.com>
2020-12-16 14:22:28 +0000

    solve for part 1

--- Day15/Day15.py
+++ Day15/Day15.py
@@ -1,16 +1,19 @@
 '''Day 15'''
 
 # PART 1
+
 def read_in_data(file) -> list:
     with open(file, 'r') as f:
         return f.readline().replace('\n', '').split(',')
 
-def next_number():
-    pass
-
 turns = read_in_data('data.txt')
 
 for i in range(len(turns), 2020):
-    pass
-print(turns)
-# {number: turn}
+    count = 0
+    for j in range(len(turns)-2, -1, -1):
+        if int(turns[i-1]) == int(turns[j]):
+            count +=(i-1)-(j)
+            break
+    turns.append(count)
+
+print(turns[-1])
