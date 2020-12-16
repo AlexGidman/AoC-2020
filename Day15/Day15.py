2d9e3c7c4ba7d8f210cc9c70fd9080484872a97b
Alex Gidman <alexgidman@outlook.com>
2020-12-16 14:48:12 +0000

    solve for part 2

--- Day15/Day15.py
+++ Day15/Day15.py
@@ -1,19 +1,25 @@
 '''Day 15'''
 
-# PART 1
+# PART 1 & 2
 
 def read_in_data(file) -> list:
     with open(file, 'r') as f:
         return f.readline().replace('\n', '').split(',')
 
+def return_turn(turn_to_return: int, turns: list) -> int:
+    '''Returns the number said on turn_to_return'''
+    for i in range(len(turns), turn_to_return):
+        count = 0
+        for j in range(len(turns)-2, -1, -1):
+            if int(turns[i-1]) == int(turns[j]):
+                count +=(i-1)-(j)
+                break
+        turns.append(count)
+    return turns[-1]
+
 turns = read_in_data('data.txt')
 
-for i in range(len(turns), 2020):
-    count = 0
-    for j in range(len(turns)-2, -1, -1):
-        if int(turns[i-1]) == int(turns[j]):
-            count +=(i-1)-(j)
-            break
-    turns.append(count)
-
-print(turns[-1])
+solution = return_turn(2020, turns)
+print(f"Part 1 Solution: {solution}")
+solution = return_turn(30000000, turns)
+print(f"Part 2 Solution: {solution}")
